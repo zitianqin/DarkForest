6e8e662bdbbad10b6ca0697f768f19217774721f
Eatheun <ethan.al469@gmail.com>
2024-03-11 20:32:10 +1100

    Added Some Way of Displaying Board

--- ui.py
+++ ui.py
@@ -1,4 +1,7 @@
 import chess
+import chess.svg
 
 board = chess.Board();
-print(board);
+with open("test1.svg", "w+") as file:
+    file.write(chess.svg.board(board));
+# I guess with this we can just open the new "test1.svg" file every game state
