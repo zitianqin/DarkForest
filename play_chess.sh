9cb69ced1dbd624393da8ca49c40bada847a29d9
Ethan Loi <ethan@9C8MY33>
2024-03-12 15:07:34 +1100

    Finally Got the UI to Draw Something

---
+++ play_chess.sh
@@ -0,0 +1,5 @@
+#! /bin/dash
+while python3 ui.py >/dev/null
+do
+	echo "success"
+done
--- ui.py
+++ ui.py
@@ -1,7 +1,52 @@
 import chess
 import chess.svg
+from tkinter import *
+import tksvg
+import pyautogui as pag
+import keyboard as kb
+import sys
+from PIL import Image, ImageTk
+from svglib.svglib import svg2rlg
+import cairosvg
+import os
 
-board = chess.Board();
-with open("test1.svg", "w+") as file:
-    file.write(chess.svg.board(board));
-# I guess with this we can just open the new "test1.svg" file every game state
+BUFF_PNG_FILE = "buff.png";
+
+class Board(chess.Board):
+    def __init__(self):
+        super().__init__();
+    
+    def writeDisplayPng(self):
+        cairosvg.svg2png(bytestring=chess.svg.board(self), write_to=BUFF_PNG_FILE);
+
+    def deleteDisplayPng(self):
+        os.remove(BUFF_PNG_FILE);
+
+class Window(Tk):
+    def __init__(self, file_path):
+        super().__init__();
+        self.size = int(pag.size()[1] * 2 / 3)
+        self.geometry(str(self.size) + "x" + str(self.size));
+        
+        # construct the label
+        self.label = Label(self);
+        self.label.pack();
+        
+    def drawBoard(self, board):
+        # grabbing the display
+        board.writeDisplayPng();
+        
+        # convert and resizing to viewport
+        boardPng = Image.open(BUFF_PNG_FILE);
+        boardPng = ImageTk.PhotoImage(boardPng.resize((self.size, self.size)));
+        
+        # draw the image
+        self.label.config(image=boardPng);
+        self.label.image = boardPng;
+        board.deleteDisplayPng();
+    
+if __name__ == "__main__":
+    win = Window("chessboard.svg");
+    board = Board();
+    win.drawBoard(board);
+    win.mainloop();
