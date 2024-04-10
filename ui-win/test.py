5d8673201989e9b4fe765cb169d6d28c080bb7a7
Eatheun <ethan.al469@gmail.com>
2024-04-10 16:48:56 +1000

    Temporary Testing File

--- ui-win/test.py
+++ ui-win/test.py
@@ -1,6 +1,15 @@
 import chess
+from tkinter import *
 
-board = chess.Board();
-print(board);
-board.set_fen("8/8/8/8/8/8/8/8");
-print(board);
+root = Tk();
+
+frame = Frame(root);
+frame.pack();
+btn1 = Button(frame);
+btn2 = Button(frame);
+btn3 = Button(frame);
+btn1.pack();
+btn2.pack();
+btn3.pack();
+
+root.mainloop();
