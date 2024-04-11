477ac19ae555d47b7fdde8450741de9e0e58fc49
Eatheun <ethan.al469@gmail.com>
2024-04-11 21:28:15 +1000

    Implemented Alpha Beta Pruning

--- py_alg/darkForest.py
+++ py_alg/darkForest.py
@@ -10,7 +10,7 @@ values = {
     "r": 5,
 }
 
-def evaluate(board):
+def evaluate(board, player):
     boardFen = str(board.board_fen());
     
     # white eval
@@ -26,21 +26,45 @@ def evaluate(board):
         if ord("a") <= ord(c) and ord(c) <= ord("z"):
             blackEval += values[c];
     
-    # white is true
-    return (whiteEval - blackEval) * (1 if board.turn else -1);
+    return whiteEval - blackEval;
 
-def evaluateRec(board, depth):
+# evaluates a position recursively and return best move with evaluation
+def minimaxPrune(board, depth, alp, bet, player):
     if (depth == 0):
-        return evaluate(board);
+        return None, evaluate(board, player);
     
-    maxEval = -inf;
+    isWhite = player == chess.WHITE;
+    bestEval = -inf if isWhite else inf;
+    compareFunc = (max if isWhite else min);
+    bestMove = None;
+    
+    # iterate
     for move in board.legal_moves:
+        # check that we have a returning move
+        if bestMove == None: bestMove = move;
+        
+        # push and evaluate
         board.push(move);
-        maxEval = max(maxEval, evaluateRec(board, depth - 1));
+        nextMove, nextEval = minimaxPrune(board, depth - 1, alp, bet, not player);
         board.pop();
 
-    return maxEval;
+        # we've found a better move
+        if nextEval != bestEval: bestMove = move;
+        bestEval = compareFunc(bestEval, nextEval);
+        
+        # pruning
+        if isWhite:
+            alp = compareFunc(alp, bestEval);
+            if alp > bestEval: break;
+        else:
+            bet = compareFunc(bet, bestEval);
+            if bet < bestEval: break;
+
+    return bestMove, bestEval;
 
 if __name__ == "__main__":
     board = chess.Board();
-    print(evaluateRec(board, 4));
+    state = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    board.set_fen(state);
+    move, eval = minimaxPrune(board, 4, -inf, inf, chess.WHITE);
+    print(str(move));
